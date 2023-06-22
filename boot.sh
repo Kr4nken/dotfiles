e1fbe4cad896e9cb6e794c785456231d64ac1d6f
Jon Jennemann <jonfelix@jennemann.com>
2023-06-22 10:54:34 +0200

    updated dotfiles

--- boot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-picom -i 1.0 -b
-killall -q polybar
-#polybar jonfelix 2>&1 | tee -a /tmp/polybar.log & disown
-feh --bg-fill /usr/share/backgrounds/norway1.jpg
-transmission-daemon
--- firstinit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash/
-sudo pacman -S btm gdu vim exa polybar awesome firefox
-sudo mkdir /usr/share/jonfelix
-mkdir ~/.config/polybar
